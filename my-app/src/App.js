2ec77ea36a52ae00b1ee5710c82d0d3b4e23dab5
Ahkeem Lang <langahkeem@gmail.com>
2017-07-20 13:36:39 -0500

    I had a syntax token error so I deleted the id property of the names array

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 var NAMES = [
   {
     name: "Ahkeem"
-    id: 1,
   },
 ];
 
@@ -23,7 +22,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Namer</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
