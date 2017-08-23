62fc1111b9c81628bd8a79a4de164e978d455566
Ahkeem Lang <langahkeem@gmail.com>
2017-08-22 20:53:05 -0500

    I will start over my application

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,55 +8,11 @@ var NAMES = [
   },
 ];
 
-class Header extends React.Component {
-  render() {
-  return(
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Welcome to Namer</h2>
-    </div>
-  )
-  };
-}
-
-class Intro extends React.Component {
-  render() {
-  return (
-    <div className="App">
-    <Header />
-      <p className="App-intro">
-        This will just be a name collector.
-      </p>
-    </div>
-  )
-  };
-}
-
-
-
-class NameTable extends React.Component {
-  render() {
-    return (
-      <div>
-        <AddNameForm />
-      </div>
-    )
-  };
-}
-
-class FilterNameTable extends React.Component {
-  render () {
-    return (
-      <Intro />
-      <NameTable />
-    )
-  };
-}
 
 class App extends Component {
   render() {
     return (
-      <FilterNameTable names={NAMES} />
+
     );
   }
 }
