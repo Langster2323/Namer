be0ef0dcd1916fcd6114021b00773ce87fcf1a53
Ahkeem Lang <langahkeem@gmail.com>
2017-08-24 22:18:06 -0500

    importing a few components from react bootstrap. created a searchform function as well

--- my-app/src/components/SearchForm.js
+++ my-app/src/components/SearchForm.js
@@ -1,9 +1,21 @@
-import React, { Component } from 'react';
-
-class SearchForm extends React.compnonent {
-  render() {
-    return (
-      
-    );
-  }
+import React from 'react';
+import {
+  Form,
+  FormGroup,
+  FormControl,
+  Button
 }
+
+const SearchForm = () => (
+  <Form inLine>
+    <FormGroup controlId="formInlineEmail">
+      <FormControl type="search" placeholder="enter something..." />
+    </FormGroup>
+    {' '}
+    <Button type="submit">
+      search
+    </Button>
+  </Form>
+);
+
+export default SearchForm;
